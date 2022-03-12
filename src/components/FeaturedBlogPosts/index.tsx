ff113e3679ca5bc23f7ddae5fcb9972851a12176
richard <richardsouzarodrigues555@gmail.com>
2022-03-12 02:16:22 -0300

    Add 'Featured blog posts' card

--- src/components/FeaturedBlogPosts/index.tsx
+++ src/components/FeaturedBlogPosts/index.tsx
@@ -1,12 +1,45 @@
+import { Link } from "react-router-dom";
+
 import styles from "./index.module.css";
 
 const FeaturedBlogPosts = () => (
   <div className={styles.container}>
-    <ul>
-      <li>Blog 1</li>
-      <li>Blog 2</li>
-      <li>Blog 3</li>
-    </ul>
+    <div className={styles.postCard}>
+      <Link to="/">
+        <h3>Post 1 - Title</h3>
+        <div>
+          <small>15 Jan, 2022</small>
+        </div>
+        <p>
+          Lorem ipsum dolor ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+          ipsum lorem ipsum lorem ipsum lorem ipsum lorem lorem lorem
+          loremloemrelorem lroe lorem loorem
+        </p>
+      </Link>
+    </div>
+    <div className={styles.postCard}>
+      <Link to="/">
+        <h3>Post 2 - Title</h3>
+        <div>
+          <small>15 Jan, 2022</small>
+        </div>
+        <p>Lorem ipsum dolor ipsum lorem ipsum lorem ipsum lorem ipsum lorem</p>
+      </Link>
+    </div>
+    <div className={styles.postCard}>
+      <Link to="/">
+        <h3>Post 3 - Title</h3>
+        <div>
+          <small>15 Jan, 2022</small>
+        </div>
+        <p>
+          Lorem ipsum dolor ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+          ipsum lorem ipsum lorem ipsum lorem ipsum lorem lorem lorem
+          loremloemrelorem lroe lorem loorem loremloemrelorem lroe lorem loorem
+          loremloemrelorem lroe lorem loorem
+        </p>
+      </Link>
+    </div>
   </div>
 );
 
