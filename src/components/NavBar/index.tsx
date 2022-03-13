3f701a8aeea9ae0b9e4dfa1ac5c82c5bbe5d9c45
richard <richardsouzarodrigues555@gmail.com>
2022-03-13 20:26:42 -0300

    Add Tooltip in navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,8 @@ import { AiFillGithub } from "react-icons/ai";
 import { CgCodeSlash, CgNotes } from "react-icons/cg";
 import { Link } from "react-router-dom";
 
+import ReactTooltip from "react-tooltip";
+
 import profilePhoto from "../../assets/profile.jpg";
 import styles from "./index.module.css";
 
@@ -9,7 +11,12 @@ const NavBar = () => (
   <div className={styles.container}>
     <nav>
       <Link to="/" className={styles.imageContainer}>
-        <img src={profilePhoto} alt="" />
+        <img
+          src={profilePhoto}
+          alt="Back to home page"
+          data-tip="Go to homepage"
+        />
+        <ReactTooltip />
       </Link>
       <ul>
         <li>
